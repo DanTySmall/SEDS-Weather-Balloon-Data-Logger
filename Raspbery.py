f1bcaed66cf3bdff7f8dd939bc7be95f51e16997
DanTySmall <Danieltysmall@knights.ucf.edu>
2024-03-25 22:13:48 -0400

    Python Source File Rough Draft

---
+++ Raspbery.py
@@ -0,0 +1,24 @@
+# SEDS - UCF
+# Weather Balloon Project
+# Raspberry Pi Source Code
+
+
+# QUESTION: The different gpio pins adhere to different standards.
+#           What Standard does each sensor adhere to
+
+
+
+# For the Sake of Development this program assumes that the Raspberry Pi is connected to:
+#     - An Altimeter
+#     - Barometer
+#     - Geiger counter
+#     - Light Sensor
+
+
+
+# Read Data From Sensors
+
+
+
+
+# Save Data To a File
